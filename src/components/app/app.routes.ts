bfe795c641d7db12fca20dc76c973be8cb1fd5ec
abc <a@a.com>
2017-07-18 14:10:13 +0900

    4.3

--- src/components/app/app.routes.ts
+++ src/components/app/app.routes.ts
@@ -5,11 +5,19 @@ import {StartComponent} from '../start/start.component';
 import {FinishComponent} from '../finish/finish.component';
 import {WorkoutHistoryComponent} from '../workout-history/workout-history.component';
 
+const workoutBuilderRoutes: Routes = [
+  {
+    path: 'builder',
+    loadChildren: 'dist/components/workout-builder/workout-builder.module#WorkoutBuilderModule'
+  }
+];
+
 export const routes: Routes = [
   { path: 'start', component: StartComponent },
   { path: 'workout', component: WorkoutContainerCompnent },
   { path: 'finish', component: FinishComponent },
   { path: 'history', component: WorkoutHistoryComponent },
+    ...workoutBuilderRoutes,
   { path: '**', redirectTo: '/start' }
 ];
 
--- src/components/workout-builder/workout-builder.module.ts
+++ src/components/workout-builder/workout-builder.module.ts
@@ -1,5 +1,5 @@
-import { NgModule }      from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule} from '@angular/common';
 
 import { ExerciseComponent } from "./exercise/exercise.component";
 import { ExercisesComponent } from "./exercises/exercises.component";
